177cd80b41b0290237e9b0f9de911ede06cd35cc
William Leacy <will.leacy10@gmail.com>
2021-03-04 22:07:30 -0500

    Add POST routes for websites and urls

--- controllers/websiteUrls.js
+++ controllers/websiteUrls.js
@@ -2,37 +2,63 @@ const express = require('express');
 const { isAuthenticated } = require('../services/middleware.js');
 const ROUTER = express.Router({mergeParams: true});
 const Projects = require('../models/projects.js')
+const Website = require('../models/websites.js')
+const WebsitesUrls = require('../models/websitesUrls.js')
 const WebsitesReports = require('./reports');
 ROUTER.use(isAuthenticated);
 ROUTER.use('/:websiteUrlId/report', WebsitesReports)
 //___________________
 // Project Routes
 //___________________
-
+ROUTER.get('/new', (req, res) => {
+  Projects.findById(req.params.projectId).then((project) => {
+    Website.findById(req.params.websiteId, (err, foundWebsite) => {
+      console.log(req.params.id);
+      console.log(foundWebsite);
+          res.render('urls/new.ejs', {
+        websites: foundWebsite,
+        projects: project
+        // websiteIndex: IndexForWebsite
+      })
+    })
+    })
+})
 
 // GET show a url
 ROUTER.get('/:id', (req, res) => {
-  Projects.findById(req.params.projectId,(err, foundProject) => {
-    let urlIndexForWebsite = 0;
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
-    console.log(`project id : ${req.params.projectId}`);
-    console.log(`website id : ${req.params.websiteId}`);
-    for (const id in foundProject.websites) {
-      console.log(foundProject.websites[id].id);
-      if (foundProject.websites[id].id == req.params.websiteId) {
-        urlIndexForWebsite = id;
-        console.log(id);
-        console.log("found");
-      } else {
-      }    
-    }
-    console.log(foundProject.websites.indexOf(req.params.websiteId));
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
-    res.render('urls/show.ejs', {
-      projects: foundProject,
-      websiteUrlIndex: urlIndexForWebsite
+
+  Projects.findById(req.params.projectId).then((foundProject) => {
+    Website.findById(req.params.websiteId).then((foundwebsite) => {
+      WebsitesUrls.findById(req.params.id, (err, foundWebsiteUrl) => {
+        console.log(req.params.id);
+        console.log(foundWebsiteUrl);
+            res.render('urls/show.ejs', {
+              websiteUrl: foundWebsiteUrl,
+              website: foundwebsite,
+              project: foundProject
+          // websiteIndex: IndexForWebsite
+        })
+      })
+      })
     })
-  })
 })
+
+// POST create a website
+ROUTER.post('/', (req, res) => {
+
+  console.log(req.body);
+
+  Website.findById(req.params.websiteId).then((project) => {
+    console.log(project)
+    console.log(req.params.websiteId)
+    WebsitesUrls.create(req.body).then(website => {
+    project.list_of_urls.push(website)
+    project.save()
+    res.send(website)
+  })
+  })
+
+})
+
 
 module.exports = ROUTER;
--- controllers/websites.js
+++ controllers/websites.js
@@ -43,62 +43,55 @@ ROUTER.get('/:id', (req, res) => {
   //       console.log("found");
   //     } else {}
   //   }
-    Projects.findById(req.params.projectId, (err, foundProject) => {
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
-    console.log(`project id : ${req.params.projectId}`);
-    console.log(`website id : ${req.params.id}`);
-    console.log(foundProject);
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
-    let IndexForWebsite = 0;
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
-    console.log(`project id : ${req.params.projectId}`);
-    console.log(`website id : ${req.params.id}`);
-    for (const id in foundProject.websites) {
-      console.log(foundProject.websites[id].id);
-      if (foundProject.websites[id].id == req.params.id) {
-        IndexForWebsite = id;
-        console.log(id);
-        console.log("found");
-      } else {}
-    }
-    res.render('websites/show.ejs', {
-      projects: foundProject,
-      websiteIndex: IndexForWebsite
+    // Projects.findById(req.params.projectId, (err, foundProject) => {
+    // console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
+    // console.log(`project id : ${req.params.projectId}`);
+    // console.log(`website id : ${req.params.id}`);
+    // console.log(foundProject);
+    // console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
+    // let IndexForWebsite = 0;
+    // console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
+    // console.log(`project id : ${req.params.projectId}`);
+    // console.log(`website id : ${req.params.id}`);
+    // for (const id in foundProject.websites) {
+    //   console.log(foundProject.websites[id].id);
+    //   if (foundProject.websites[id].id == req.params.id) {
+    //     IndexForWebsite = id;
+    //     console.log(id);
+    //     console.log("found");
+    //   } else {}
+    // }
+    // res.render('websites/show.ejs', {
+    //   projects: foundProject,
+    //   websiteIndex: IndexForWebsite
+    // })
+  // })
+
+  Projects.findById(req.params.projectId).then((project) => {
+    Website.findById(req.params.id, (err, foundWebsite) => {
+      console.log(req.params.id);
+      console.log(foundWebsite);
+          res.render('websites/show.ejs', {
+        websites: foundWebsite,
+        projects: project
+        // websiteIndex: IndexForWebsite
+      })
     })
-  })
+    })
+
+
 })
 
 // POST create a website
 ROUTER.post('/', (req, res) => {
 
   console.log(req.body);
-  // Website.create(req.body, (error, createdWebsite) => {
-  //   res.send(createdWebsite);
-  // })
-  // let website = new Website({website_name: "test"});
-  // console.log(website);
-  // Projects.findById(req.params.projectId, {
-  //     '$push': {
-  //       websites: website
-  //     }
-  //   }, {
-  //     'new': true,
-  //     'runValidators': true
-  //   },
-  //   function (err, result) {
 
-
-  //     if (err) {
-  //       res.send(err)
-  //     } else {
-  //       res.send(result)
-  //     }
-  //   });
   Projects.findById(req.params.projectId).then((project) => {
   Website.create(req.body).then(website => {
     project.websites.push(website)
     project.save()
-    res.send(project)
+    res.send(website)
   })
   })
 
@@ -140,14 +133,16 @@ ROUTER.delete('/:id', (req, res) => {
   console.log("Deleting website");
   console.log(`~~~~~~~~~~~~~~~~~~~~~~`);
 
-  Website.findByIdAndRemove(req.params.id
-  , {
-    useFindAndModify: false
-  }, (err, data) => {
-    console.log(req.params.id);
-    res.redirect(`/projects/${req.params.projectId}`)
-  })
-})
+  
 
-;
+
+
+  
+    Website.findByIdAndRemove(req.params.id, { useFindAndModify: false }, (err, data)=>{
+      console.log(req.params.id);
+      res.redirect('/projects')
+    })
+
+  })
+
 module.exports = ROUTER;
